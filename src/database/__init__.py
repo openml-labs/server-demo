1fc1b74f50f066f94ee1f4d3ae339f0d26ebb67b
Jos van der Velde <j.d.v.d.velde@tue.nl>
2023-01-12 11:23:26 +0100

    Removed initialization of database in database/__init__.py. I think that this is unnecessary

--- src/database/__init__.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    from setup import connect_to_database, populate_database
-
-    engine = connect_to_database(
-        "mysql://root:ok@127.0.0.1:3307/aiod",
-        delete_first=True,
-    )
-    populate_database(engine)
